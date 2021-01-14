2ed957444bf89a4c491f05d69fcbfe03563fe0b6
Aleksandr18 <aleksandrperetertov1@gmail.com>
2021-01-14 14:09:25 +0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,15 @@
 
 $(document).ready(function () {
-    $(document).on("click", '.burger', function () {
-        $('body').toggleClass('modal-open');
+
+
+    $('.owl-Img').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout : 7000,
+        margin: 24,
+        nav: true,
+        items: 1
     });
-
-    var elem = $('header');
-    var doc = $(document);
-
-    function scrolled() {
-        var threshold = doc.scrollTop() > 50;
-        elem.toggleClass('scrolled', threshold);
-    }
-
-    if(scrolled < 50) {
-        elem.addClass(scrolled);
-    } else {
-        elem.removeClass(scrolled);
-    }
-
-    $(window).on({scroll: scrolled});
 
 
 
